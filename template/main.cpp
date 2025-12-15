5145d7327f48bef5005d4d0e2611b13d5dded911
ktsn-ud <153289885+ktsn-ud@users.noreply.github.com>
2025-12-15 22:32:11 +0900

    add: 競プロ用テンプレートを配置

---
+++ template/main.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+#define el '\n'
+
+template <typename T>
+inline void print_vec(const vector<T> &v, bool split_line = false) {
+  const char sep = split_line ? '\n' : ' ';
+  for (int i = 0; i < (int)v.size(); i++) {
+    if (i)
+      cout << sep;
+    cout << v[i];
+  }
+  cout << '\n';
+}
+
+// --- solution from here ---
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  return 0;
+}
